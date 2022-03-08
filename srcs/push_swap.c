90283f323c7f0ae82f94c6ff39e2648a46c4ccca
Navi <mailvoornavi@gmail.com>
2022-03-08 17:37:08 +0100

    added push_swap initialize function

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -13,6 +13,22 @@
 #include <push_swap.h>
 
 /**
+ * Initialize the t_push_swap struct
+ *
+ * @return  t_push_swap
+ */
+static t_push_swap *initialize()
+{
+	t_push_swap	*push_swap;
+
+	push_swap = (t_push_swap *)malloc(sizeof(t_push_swap));
+	push_swap->stack_a = (t_stack *)malloc(sizeof(t_stack));
+	push_swap->stack_b = (t_stack *)malloc(sizeof(t_stack));
+	
+	return (push_swap);
+}
+
+/**
  * This project will sort data on a stack, with a limited set of instructions,
  * using the lowest possible number of actions. For this project, you have to
  * manipulate various types of algorithms and choose the one (of many) most
@@ -20,10 +36,11 @@
  */
 int	main(int argc, char **args)
 {
-	t_push_swap	push_swap;
+	t_push_swap	*push_swap;
 	
 	(void)argc;
 	(void)args;
+	push_swap = initialize();
 	stack_debug_print(push_swap, push_swap->stack_a);
 	return (0);
 }
