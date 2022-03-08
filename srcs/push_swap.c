fa7bd99a97ea2ef0c2fac99eb2214ef56e30447e
Navi <mailvoornavi@gmail.com>
2022-03-08 17:37:27 +0100

    added stack_initialize functionality
    - lists the arguments and puts them in stack_a

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/03/06 16:45:34 by navi          ########   odam.nl         */
+/*   Updated: 2022/03/08 17:36:48 by navi          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,39 @@ static t_push_swap *initialize()
 }
 
 /**
+ * Initialize the push_swap->stack_a struct
+ *
+ * @param   char  lst
+ * @param   int   n
+ *
+ * @return  void
+ */
+static void stack_initialize(t_push_swap *push_swap, char **lst, int n)
+{
+	t_stack		*new_stack;
+	int			i;
+	int			nbr;
+
+	i = 1;
+	while (i < n)
+	{
+		nbr = ft_atoi(lst[i]);
+		if (ft_isaldigit(lst[i]) == 1)
+		{
+			new_stack = stack_create_item(nbr);
+			if (!new_stack)
+				error("Memory error!");
+			stack_push(push_swap, &push_swap->stack_a, new_stack);
+		}
+		else
+		{
+			error("Invalid arguments");
+		}
+		i++;
+	}
+}
+
+/**
  * This project will sort data on a stack, with a limited set of instructions,
  * using the lowest possible number of actions. For this project, you have to
  * manipulate various types of algorithms and choose the one (of many) most
@@ -38,9 +71,8 @@ int	main(int argc, char **args)
 {
 	t_push_swap	*push_swap;
 	
-	(void)argc;
-	(void)args;
 	push_swap = initialize();
+	stack_initialize(push_swap, args, argc);
 	stack_debug_print(push_swap, push_swap->stack_a);
 	return (0);
 }
